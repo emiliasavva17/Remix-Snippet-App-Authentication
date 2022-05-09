3bd4561b21e25768f7b7e3cc48d7128a469cc4ad
Emilia Savva <emiliasavva.2000@gmail.com>
2022-05-09 13:02:20 +0200

    Migrate to Remix 1.4.3

--- app/routes/logout.jsx
+++ app/routes/logout.jsx
@@ -1,5 +1,5 @@
 import { getSession, destroySession } from "~/sessions.server";
-import { redirect } from "remix";
+import { redirect } from "@remix-run/node";
 
 export async function action({ request }) {
   const session = await getSession(request.headers.get("Cookie"));
